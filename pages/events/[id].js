1a258d4d538bc5ed8a521e75ef422e6fb92101f3
Pedro Tigre <pedrogtigre@gmail.com>
2022-07-28 00:04:09 -0300

    Database added

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -2,13 +2,15 @@ import { useRouter } from 'next/router';
 import EventContent from '../../components/event-detail/event-content';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
+import clientPromise, { getDataById } from '../../util/mongodb';
+// import { getEventById } from '../../dummydata';
 
-import { getEventById } from '../../dummydata';
-
-function SomeId() {
+function SomeId(props) {
   const router = useRouter();
   const id = router.query.id;
-  const event = getEventById(id);
+
+  const { data } = props;
+  const event = data;
 
   if (!id) return null;
   if (event === undefined) {
@@ -29,6 +31,25 @@ function SomeId() {
       </EventContent>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  try {
+    const client = await clientPromise;
+
+    const data = await getDataById(client, params.id);
+
+    return {
+      props: { data, isConnected: true },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { isConnected: false },
+    };
+  }
 }
 
 export default SomeId;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,38 @@
-import { getFeaturedEvents } from '../dummydata';
+// import { getFeaturedEvents } from '../dummydata';
 import EventList from '../components/events/event-list';
+import clientPromise, { getFeaturedData } from '../util/mongodb';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+  const { isConnected, data } = props;
+  // console.log(isConnected, data);
+
+  // getting data from mongodb
+
+  const featuredEvents = data;
 
   return (
     <>
       <EventList items={featuredEvents} />
     </>
   );
+}
+
+export async function getStaticProps(context) {
+  try {
+    const client = await clientPromise;
+
+    const data = await getFeaturedData(client);
+
+    return {
+      props: { data, isConnected: true },
+      revalidate: 10,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { isConnected: false },
+    };
+  }
 }
 
 export default HomePage;
